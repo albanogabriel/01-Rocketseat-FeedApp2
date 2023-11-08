8247e07cdf2cf63a361fa629b83bd3c876fa6d70
Gabriel Albano <albanogabriel33@gmail.com>
2023-11-08 15:58:54 -0300

    trabalhando na sessão Post feito o hover do focus

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -11,17 +11,32 @@ export function Post() {
                             <strong>Gabriel Albano</strong>
                             <span>Front-end Developer</span>
                         </div>
-
-                        <time title="11 de maio às 08:13h" dateTime="2022-05-11 08:13:30">Publicado há 1h</time>
                     </div>
+
+                    <time title="11 de maio às 08:13h" dateTime="2022-05-11 08:13:30">Publicado há 1h</time>
                 </header>
 
                 <div className={styles.content}>
                     <p>Fala Galera 👏</p>
                     <p> acabei de subir mais um projeto no meu portfia. É um projeto que fiz no NLW Return, evento da Rocket</p>
-                    <p>👉 <a href="">jane.design/doctorcare</a></p>
-                    <p><a href="">#novoprojeto #nwl #rocketseat</a></p>
+                    <p><a href="">jane.design/doctorcare</a></p>
+                    <p>
+                        <a href="">#novoprojeto</a>{' '}
+                        <a href="">#nwl</a>{' '}
+                        <a href="">#rocketseat</a>{''}
+                    </p>
                 </div>
+
+                <form className={styles.commentForm}>
+                    <strong>Deixa seu feedback</strong>
+                    <textarea placeholder="deixe um comentário"/>
+
+                    <footer>
+                        <button type="submit">Publicar</button>
+                    </footer>
+                </form>
+                
+                
             </article>
         </div>
     )
