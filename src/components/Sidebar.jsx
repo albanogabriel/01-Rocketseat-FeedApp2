124bbf84e0ab9f0fe39675e8cf5bca5c8d63c58a
Gabriel Albano <albanogabriel33@gmail.com>
2023-11-08 10:54:05 -0300

    terminado de atualizar os erros da aplicação

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,10 @@
+import { PencilLine } from 'phosphor-react'
+
 import styles from './Sidebar.module.css'
 
 export function Siderbar() {
     return (
-        <aside className={styles.siderbar}>
+        <aside className={styles.sidebar}>
             <img className={styles.cover} src="https://images.unsplash.com/photo-1698308246064-e86c57c63782?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
 
             <div className={styles.profile}>
@@ -14,6 +16,7 @@ export function Siderbar() {
 
             <footer>
                 <a href="#">
+                    <PencilLine size={20}/>
                     Editar seu perfil
                 </a>
             </footer>
