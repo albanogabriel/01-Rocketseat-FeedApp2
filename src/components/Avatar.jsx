da09dbfc8002dfe4cd9646d4f6edb5e58ef006a7
Gabriel Albano <albanogabriel33@gmail.com>
2023-11-14 10:06:24 -0300

    fix: corrigido letra faltando

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -12,9 +12,7 @@ export function Avatar( { hasBorder = true, src } ) {
 
   return (
     <img 
-        // eslint-disable-next-line react/prop-types
         className={hasBorder ? styles.avatarWithBorder : styles.avatar }
-        // eslint-disable-next-line react/prop-types
         src={src} 
     />
   )
