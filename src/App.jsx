8db2c7e0ac516d585d55f5902e7061595d9da3de
Gabriel Albano <albanogabriel33@gmail.com>
2023-11-16 00:44:37 -0300

    FEAT: incluindo eslint, prettier .vscode, para correção do código

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 /* eslint-disable no-undef */
-import { Header } from "./components/Header"
+import { Header } from './components/Header'
 
-import styles from "./App.module.css"
+import styles from './App.module.css'
 
 import './global.css'
-import { Siderbar } from "./components/Sidebar"
-import { Post } from "./components/Post"
+import { Siderbar } from './components/Sidebar'
+import { Post } from './components/Post'
 
 // O que vamos precisar ?
 // author: { avatar_Url: "", name: "", role: "" }
@@ -17,55 +17,53 @@ import { Post } from "./components/Post"
 
 const posts = [
   {
-      id: 1,
-      author: {
-          avatarUrl: "https://github.com/albanogabriel.png",
-          name: "Gabriel Albano",
-          role: "SCRUM MASTER / Front-end Developer"
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/albanogabriel.png',
+      name: 'Gabriel Albano',
+      role: 'SCRUM MASTER / Front-end Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala Galera 👏,' },
+      {
+        type: 'paragraph',
+        content: 'acabei de subir mais um projeto no meu portfia, É um projeto que fiz no NLW Return, evento da Rocket'
       },
-      content: [
-          { type: 'paragraph', content: 'Fala Galera 👏,' },
-          { type: 'paragraph', content: 'acabei de subir mais um projeto no meu portfia, É um projeto que fiz no NLW Return, evento da Rocket'},
-          { type: 'link', content: 'jane.design/doctorcare' }
-      ],
-      publishedAt: new Date('2022-05-03 20:00:00'),
+      { type: 'link', content: 'jane.design/doctorcare' }
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00')
   },
   {
-      id: 2,
-      author: {
-          avatarUrl: "https://github.com/diego3g.png",
-          name: "Diego Fernandes",
-          role: "CTO @rocketseat"
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala Galera 👏,' },
+      {
+        type: 'paragraph',
+        content: 'acabei de subir mais um projeto no meu portfia, É um projeto que fiz no NLW Return, evento da Rocket'
       },
-      content: [
-          { type: 'paragraph', content: 'Fala Galera 👏,' },
-          { type: 'paragraph', content: 'acabei de subir mais um projeto no meu portfia, É um projeto que fiz no NLW Return, evento da Rocket'},
-          { type: 'link', content: 'jane.design/doctorcare' }
-      ],
-      publishedAt: new Date('2022-05-10 19:00:00'),
+      { type: 'link', content: 'jane.design/doctorcare' }
+    ],
+    publishedAt: new Date('2022-05-10 19:00:00')
   }
 ]
 
 function App() {
   return (
     <div>
-      <Header/>
+      <Header />
 
       <div className={styles.wrapper}>
         <Siderbar />
         <main>
           {/* Renderizar meu componente Post */}
-          {posts.map(post => {
-            return (
-              <Post 
-                key={post.id}
-                author={post.author}
-                content={post.content}
-                publishedAt={post.publishedAt}
-              />
-            )
+          {posts.map((post) => {
+            return <Post key={post.id} author={post.author} content={post.content} publishedAt={post.publishedAt} />
           })}
-          
         </main>
       </div>
     </div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/jsx-key */
+/* eslint-disable react/prop-types */
 import { format, formatDistanceToNow } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
 
@@ -8,80 +10,80 @@ import styles from './Post.module.css'
 import { useState } from 'react'
 
 export function Post({ author, publishedAt, content }) {
-    const [comments, setComments] = useState([
-        'Post muito bacana, hein!'
-    ])
+  const [comments, setComments] = useState(['Post muito bacana, hein!'])
 
-    const [ newCommentText, setNewCommentText ] = useState('')
+  const [newCommentText, setNewCommentText] = useState('')
 
-    const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
-        locale: ptBR
-    })
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+    locale: ptBR
+  })
 
-    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
-        locale: ptBR,
-        addSuffix: true
-    })
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true
+  })
 
-    function handleAddComment() {
-        event.preventDefault()
+  function handleAddComment() {
+    event.preventDefault()
+    setComments([...comments, newCommentText])
+    setNewCommentText('')
+  }
 
-        setComments([...comments, newCommentText])
-        setNewCommentText('')
-    }
+  function handleNewCommentChange() {
+    //se eu quero adicionar esse valor ao meu estado:
+    setNewCommentText(event.target.value)
+  }
 
-    function handleNewCommentChange() {
-        //se eu quero adicionar esse valor ao meu estado:
-        setNewCommentText(event.target.value)
-    }
-    
-    return (
-        <article className={styles.post}>
-            <header>
-                <div className={styles.author}>
-                    <Avatar src={author.avatarUrl}/>
-                    <div className={styles.authorInfo}>
-                        <strong>{author.name}</strong>
-                        <span>{author.role}</span>
-                    </div>
-                </div>
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <Avatar src={author.avatarUrl} />
+          <div className={styles.authorInfo}>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
+          </div>
+        </div>
 
-                <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
-                    {publishedDateRelativeToNow}
-                </time>
-            </header>
+        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+          {publishedDateRelativeToNow}
+        </time>
+      </header>
 
-            <div className={styles.content}>
-                
-                    {content.map(item => {
-                        if (item.type === 'paragraph') {
-                            return <p>{item.content}</p>
-                        } else if (item.type === 'link') {
-                            return <p><a href="">{item.content}</a></p>
-                        }
-                    })}
+      <div className={styles.content}>
+        {content.map((item) => {
+          if (item.type === 'paragraph') {
+            return <p>{item.content}</p>
+          } else if (item.type === 'link') {
+            return (
+              <p>
+                <a href="">{item.content}</a>
+              </p>
+            )
+          }
+        })}
 
-                    <form onSubmit={handleAddComment} className={styles.commentForm}>
-                        <strong>Deixa seu feedback</strong>
-                        <textarea 
-                            name="comment" 
-                            placeholder="deixe um comentário"
-                            value={newCommentText}
-                            onChange={handleNewCommentChange}
-                        />
-                        <footer>
-                            <button type="submit">Publicar</button>
-                        </footer>
-                    </form>
+        <form onSubmit={handleAddComment} className={styles.commentForm}>
+          <strong>Deixa seu feedback</strong>
+          <textarea
+            name="comment"
+            placeholder="deixe um comentário"
+            value={newCommentText}
+            onChange={handleNewCommentChange}
+          />
+          <footer>
+            <button type="submit">Publicar</button>
+          </footer>
+        </form>
 
-                    <div className={styles.commentList}>
-                        {comments.map(comment => {
-                            //o comment é = o valor no array = 'Post muito bacana, hein!'
-                            // 'Post muito bacana, hein!' é passado como props
-                            return <Comment content={comment} />
-                        })}
-                    </div>  
-                </div>                        
-        </article>
-    )
+        <div className={styles.commentList}>
+          {comments.map((comment) => {
+            //o comment é = o valor no array = 'Post muito bacana, hein!'
+            // 'Post muito bacana, hein!' é passado como props
+            return <Comment content={comment} />
+          })}
+        </div>
+      </div>
+    </article>
+  )
 }
